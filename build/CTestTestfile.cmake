430e8d16d350e8f4d8649706fa6ef4ba980a243e
Shriram <dshriram27@gmail.com>
2022-11-10 16:48:53 +0530

    commit1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/e/arduinobot_ws/src
+# Build directory: /mnt/e/arduinobot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arduinobot_description")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/mnt/e/arduinobot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/mnt/e/catkin_ws2/devel;/opt/ros/noetic'.split(';') if '/mnt/e/catkin_ws2/devel;/opt/ros/noetic' != '' else []
